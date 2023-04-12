ad622724bd44c094efdc1faf29daf860737e9bac
LEEGURTS <rmstjd333@gmail.com>
2023-04-12 21:07:59 +0900

    [FE DEV] 적응형 해상도 사용, 마커 회전

--- client/src/components/FindLocation/FindLocationImageHandle.tsx
+++ client/src/components/FindLocation/FindLocationImageHandle.tsx
@@ -11,12 +11,12 @@ const FindLocationImageHandle: React.FunctionComponent = () => {
   const [analyzeResultList, setAnalyzeResultList] = useState<Coordinate[]>([]);
 
   // 좌표 계산 함수 -> 분석 결과로 대체하기
-  const getCoordinate = (length: number) => {
-    return Array(length)
+  const getCoordinate = () => {
+    return Array(Math.ceil(Math.random() * 3 + 1))
       .fill(0)
-      .map((_, i) => ({
-        lat: 0.1 * i + 37.5,
-        lng: 0.1 * i + 126.988259,
+      .map(() => ({
+        lat: Math.random() / 100 + 37.588556,
+        lng: Math.random() / 100 + 127.019981,
       }));
   };
 
@@ -45,9 +45,7 @@ const FindLocationImageHandle: React.FunctionComponent = () => {
         imageList={imageList}
         setImageList={setImageList}
       />
-      <GoogleMapApi
-        coordinate={imageList.map((_, idx) => getCoordinate(idx + 1))}
-      />
+      <GoogleMapApi coordinate={imageList.map((_) => getCoordinate())} />
     </>
   );
 };
